8d277f5fee2df6a91cf0b95dfd8b006a6babef57
jenny <jenny.young@kakaomobility.com>
2019-11-17 14:18:10 +0900

    commit#1

--- app/src/main/java/com/bubu/androidtest/MainActivity.kt
+++ app/src/main/java/com/bubu/androidtest/MainActivity.kt
@@ -2,11 +2,14 @@ package com.bubu.androidtest
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tv_hello.text = "전국진바보"
     }
 }
